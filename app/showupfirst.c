341d1ff9d2aaa343d74044cf4d835bd7cea056ad
ksvetova <58701924+ksvetova@users.noreply.github.com>
2020-05-08 14:52:29 +0300

    Update showupfirst.c

--- app/showupfirst.c
+++ app/showupfirst.c
@@ -40,12 +40,12 @@ static void showupfirst_line(int index, char *contents, int cursor, void *data)
     /* Капитализация символов */
     char line[MAXLINE];
     char output_line[MAXLINE];
-    strcpy(line, contents);
+    strncpy(line, contents, MAXLINE);
 
     if (cursor > 0){
         strncpy(output_line, line, (unsigned long)cursor);
         output_line[cursor] = '|';
-        strcpy(output_line+cursor+1, line+cursor);
+         strncpy(output_line + cursor + 1, line + cursor, MAXLINE - cursor - 1);
         toup(output_line);
         printf("%s", output_line);
     } else {
